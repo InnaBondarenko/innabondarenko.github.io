5094ebe174a449a03b68e3e86610b1df05bf0434
Inna_B <bondarenkoinnaa@gmail.com>
2019-10-30 18:38:36 +0200

    changed cv 10/2019

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ $(".menu-main").on("click","a", function (event) {
 	event.preventDefault();
 	var id  = jQuery(this).attr('href');
 	var top = jQuery(id).offset().top;
-	$('body,html').animate({scrollTop: top}, 1500);
+	$('.menu.menu-open').removeClass('menu-open');
+	$('body,html').animate({scrollTop: top - 40}, 800);
 });
 
 $('.menu-icon').on('click', function() {
@@ -113,4 +114,14 @@ $('.inline-popup').magnificPopup({
 			$(form).find('button[type="submit"]').html('Не вышло :(');
 		});
 	}
+});
+
+$(document).on('click', function(e) {
+
+	if(e.target.className === "menu" || !$(e.target).closest('.menu-icon').length == 0) {
+		return false;
+	}else {
+		$('.menu.menu-open').removeClass('menu-open');
+	}
+
 });
